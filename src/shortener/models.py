45001aad735369ac93804bf994ad67097696e533
Jaroslove <jroslovemonro@gmail.com>
2017-04-23 15:21:54 -0700

    to mi

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class KirrURL(models.Model):
 	"""docstring for KirrURL"""
 	url = models.CharField(max_length=220)
-
+	shortcode = models.CharField(max_length=15, unique=True)
 	def __str__(self):
 		return str(self.url)
 	def __unicode__(self):
